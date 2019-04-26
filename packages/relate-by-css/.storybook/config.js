a170311c251cdda143cac8dc9d6a0aedc80dbe16
Andreas Kollegger <akollegger@gmail.com>
2019-04-26 11:59:22 +0200

    port of semantic-ui customized to use Open Sans and with a storybook preview

---
+++ packages/relate-by-css/.storybook/config.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+import '../semantic/dist/relate-by.css';
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../stories', true, /\.stories\.js$/);
+
+addDecorator(withInfo); //
+
+addDecorator(storyFn => <div className='ui container'>{storyFn()}</div>);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
---
+++ packages/relate-by-css/readme.md
@@ -0,0 +1,23 @@
+# Relate By CSS
+### _a customized Semantic UI CSS framework_
+
+
+## Explore
+
+Preview locally: `npm run storybook`
+
+
+## Build
+
+Initialize: `npm i`
+
+Build: `npm run build`
+
+Develop: `npm run dev`
+
+Output: `dist/relate-by.css`
+
+## Theming Guide
+
+Read the (Semantic UI Theming Guide)[https://semantic-ui.com/usage/theming.html]
+
