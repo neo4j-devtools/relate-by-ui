d5c99ad7fc7cef3476e5d06054aafba5e71a4aaf
Linus Lundahl <lundahl.linus@gmail.com>
2019-05-09 14:01:06 +0200

    Fix styleguide storybook and css build-issues.

--- packages/styleguide/.storybook/config.js
+++ packages/styleguide/.storybook/config.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 
-import '~@relate-by-ui/css/semantic/dist/relate-by.min.css';
+import '@relate-by-ui/css/semantic/dist/relate-by.min.css';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
