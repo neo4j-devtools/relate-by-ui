01e67a1625a3ca57487208096765ad06179c1879
Andreas Kollegger <akollegger@gmail.com>
2019-05-10 09:16:02 +0200

    reorganized the css container stories into subgroups

--- packages/css/stories/element/element-container.stories.js
+++ packages/css/stories/element/element-container.stories.js
@@ -48,3 +48,67 @@ stories.add('Fluid', () => (
     </div>
   </div>
 ));
+
+const exampleStories = storiesOf('Element/Container/Examples', module);
+
+exampleStories.add('Using Grids', () => (
+  <div class="ui four column doubling stackable grid container">
+    <div class="column">
+      <p><MediaBlock color="#E0E0E0" rows={4} /></p>
+      <p><TextBlock style={{height:'4em'}} color='#E0E0E0' rows={4} /></p>
+      <p><TextBlock style={{height:'4em'}} color='#E0E0E0' rows={4} /></p>
+    </div>
+    <div class="column">
+      <p><MediaBlock color="#E0E0E0" rows={4} /></p>
+      <p><TextBlock style={{height:'4em'}} color='#E0E0E0' rows={4} /></p>
+      <p><TextBlock style={{height:'4em'}} color='#E0E0E0' rows={4} /></p>
+    </div>
+    <div class="column">
+      <p><MediaBlock color="#E0E0E0" rows={4} /></p>
+      <p><TextBlock style={{height:'4em'}} color='#E0E0E0' rows={4} /></p>
+      <p><TextBlock style={{height:'4em'}} color='#E0E0E0' rows={4} /></p>
+    </div>
+    <div class="column">
+      <p><MediaBlock color="#E0E0E0" rows={4} /></p>
+      <p><TextBlock style={{height:'4em'}} color='#E0E0E0' rows={4} /></p>
+      <p><TextBlock style={{height:'4em'}} color='#E0E0E0' rows={4} /></p>
+    </div>
+  </div>
+));
+
+exampleStories.add('Centered Menu', () => (
+  <div class="ui attached stackable menu">
+    <div class="ui container">
+      <a class="item">
+        <i class="home icon"></i> Home
+      </a>
+      <a class="item">
+        <i class="grid layout icon"></i> Browse
+      </a>
+      <a class="item">
+        <i class="mail icon"></i> Messages
+      </a>
+      <div class="ui simple dropdown item">
+        More
+        <i class="dropdown icon"></i>
+        <div class="menu">
+          <a class="item"><i class="edit icon"></i> Edit Profile</a>
+          <a class="item"><i class="globe icon"></i> Choose Language</a>
+          <a class="item"><i class="settings icon"></i> Account Settings</a>
+        </div>
+      </div>
+      <div class="right item">
+        <div class="ui input"><input type="text" placeholder="Search..."/></div>
+      </div>
+    </div>
+  </div>
+));
+
+
+exampleStories.add('Centered Menu', () => (
+  <div class="ui raised very padded text container segment">
+    <h2 class="ui header">Dogs Roles with Humans</h2>
+    <p>{faker.lorem.lines(8)}</p>
+    <p>{faker.lorem.lines(12)}</p>
+  </div>
+));
