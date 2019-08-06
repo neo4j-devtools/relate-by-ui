de314bf463f1364bfd28b7009c7ba5de64070013
Hugo Bove <hugo.bove@neotechnology.com>
2019-08-06 11:40:46 +0200

    more exports

--- packages/saved-scripts/src/index.ts
+++ packages/saved-scripts/src/index.ts
@@ -1,3 +1,11 @@
 import SavedScripts from './saved-scripts';
 
 export default SavedScripts;
+export {
+  addScriptPathPrefix,
+  getScriptDisplayName,
+  sortAndGroupScriptsByPath,
+} from './saved-scripts.utils';
+export {
+  arrayHasItems,
+} from './generic.utils';
