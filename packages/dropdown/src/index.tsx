8949c4f13d750d043a34a516d2eddfab96a2cd27
Linus Lundahl <lundahl.linus@gmail.com>
2019-08-22 16:09:56 +0200

    Add Dropdown.

---
+++ packages/dropdown/src/index.tsx
@@ -0,0 +1,9 @@
+import Dropdown from './Dropdown';
+
+import { RelateByUIDropdownPropTypes, RelateByUIDropdownItemPropTypes } from './Types';
+
+export {
+  Dropdown,
+  RelateByUIDropdownPropTypes,
+  RelateByUIDropdownItemPropTypes
+};
